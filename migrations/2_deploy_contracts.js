4ecf9bdd509bae938d578f7052ae799db5f9d0e3
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-10-02 23:31:56 -0300

    include all neccesary deployments chained together

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,17 @@
-const MultisigWalletWithDailyLimit = artifacts.require('MultiSigWalletWithDailyLimit.sol')
-const MultisigWalletWithoutDailyLimit = artifacts.require('MultiSigWallet.sol')
-const MultisigWalletFactory = artifacts.require('MultiSigWalletWithDailyLimitFactory.sol')
+const MultiSigWallet = artifacts.require('MultiSigWallet.sol')
+const MultiSigWalletFactory = artifacts.require('MultiSigWalletFactory.sol')
+const ValorTokenMockup = artifacts.require('ValorTokenMockup')
+const ValorStakeFactory = artifacts.require('ValorStakeFactory')
 
-module.exports = deployer => {
-  const args = process.argv.slice()
-  if (process.env.DEPLOY_FACTORY){
-    deployer.deploy(MultisigWalletFactory)
-    console.log("Factory with Daily Limit deployed")
-  } else if (args.length < 5) {
-    console.error("Multisig with daily limit requires to pass owner " +
-      "list, required confirmations and daily limit")
-  } else if (args.length < 6) {
-    deployer.deploy(MultisigWalletWithoutDailyLimit, args[3].split(","), args[4])
-    console.log("Wallet deployed")
-  } else {
-    deployer.deploy(MultisigWalletWithDailyLimit, args[3].split(","), args[4], args[5])
-    console.log("Wallet with Daily Limit deployed")
-  }
+module.exports = (deployer, networks, accounts)=> {
+  // deploy the Factory to specify at dapp
+  deployer.deploy(MultiSigWalletFactory).then(function (){
+    // deploy multiSig Wallet
+    return deployer.deploy(MultiSigWallet, [accounts[0], accounts[1], accounts[2]], 2)
+  }).then(function (){
+    // deploy ValorToken Mockup
+    return deployer.deploy(ValorTokenMockup, accounts[0], accounts[1], accounts[2])
+  }).then (function (){
+    return deployer.deploy(ValorStakeFactory, ValorTokenMockup.address, MultiSigWallet.address)
+  })
 }
